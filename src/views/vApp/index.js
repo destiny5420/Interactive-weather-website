5a2c9d86a7692045ea0b04a769b364deca949cbe
paper.window <destiny5420@gmail.com>
2020-09-17 22:03:26 +0800

    [Add] 新增氣溫調節曲線圖

--- src/views/vApp/index.js
+++ src/views/vApp/index.js
@@ -2,18 +2,41 @@ export default {
   name: 'vApp',
   props: {},
   // components: {},
-  // data: function() {
-  //   return {};
+  data: function() {
+    return {
+      points: [
+        { x: 40, value: 20 },
+        { x: 90, value: 23 },
+        { x: 140, value: 28 },
+        { x: 190, value: 27.5 },
+        { x: 240, value: 22 },
+        { x: 290, value: 29 },
+        { x: 340, value: 20 },
+      ],
+    };
+  },
+  // methods: {
+
   // },
-  // methods: {},
-  // computed: {},
+  computed: {
+    tmpLineY: {
+      get: function() {
+        const vm = this;
+        return function(index) {
+          const temUnit = Math.abs(40 - 10);
+          const heightUnit = Math.abs(50 - 120);
+          return 100 - (vm.points[index].value / temUnit) * heightUnit;
+        };
+      },
+    },
+  },
   // // life cycle
-  // beforeCreate: function() {},
-  // created: function() {},
-  // beforeMounted: function() {},
-  // mounted: function() {},
-  // beforeUpdate: function() {},
-  // updated: function() {},
-  // beforeDestroy: function() {},
-  // Destroy: function() {},
+  beforeCreate: function() {},
+  created: function() {},
+  beforeMounted: function() {},
+  mounted: function() {},
+  beforeUpdate: function() {},
+  updated: function() {},
+  beforeDestroy: function() {},
+  Destroy: function() {},
 };
